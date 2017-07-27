502c954e6545e3557885b9bfe893c2339a6c9e49
yolo <kuroikenshi@sina.com>
2017-07-27 15:29:41 +0800

    Up fw7-fb, change data

--- ws/form_configurator/assets/fw7/js/my-app-form-builder.js
+++ ws/form_configurator/assets/fw7/js/my-app-form-builder.js
@@ -35,26 +35,12 @@ var pageTitles = [
         url: 'student-2',
         label: '学生 - 2'
     }];
-// 当前页面的页码
-var nowPageIndex = 0;
 
-// 初始化页面和导航
-$$.each(pageTitles, function(idx){
-    var pt = pageTitles[idx];
-
-    // 导航菜单初始化
-    $$('.navList').append('<p><a class="close-panel" href="#' + pt.url + '">' + pt.label + '</a></p>');
-
-    // page初始化
-    var $page = $$(pageTemplate.format({formId: pt.url})).attr('data-page', pt.url)
-            .attr('page-index', idx).addClass('cached');
-
-    // TODO: 初始化页面详细过程
-    if (idx != 0) {
-        $$('.pages').append($page);
-
-        $page.find('form').renderForm({
-            "items": [{
+// 表单数据
+var jsonConfs = {
+    'student-1':{
+        "items": [
+            {
                 "name": "s1",
                 "label": "下拉单选",
                 "outerWidth": 12,
@@ -68,17 +54,14 @@ $$.each(pageTitles, function(idx){
                 "options": [{
                     "label": "选项1",
                     "value": 1
-                },
-                {
+                }, {
                     "label": "选项2",
                     "value": 2
-                },
-                {
+                }, {
                     "label": "选项3",
                     "value": 3
                 }]
-            },
-            {
+            }, {
                 "name": "t1",
                 "label": "文本框",
                 "outerWidth": 12,
@@ -88,8 +71,7 @@ $$.each(pageTitles, function(idx){
                 "type": "text",
                 "placeholder": "请输入文本",
                 "description": "请输入英文、数字、下划线"
-            },
-            {
+            }, {
                 "name": "s2",
                 "label": "下拉多选",
                 "outerWidth": 12,
@@ -112,8 +94,7 @@ $$.each(pageTitles, function(idx){
                     "label": "选项3",
                     "value": 3
                 }]
-            },
-            {
+            }, {
                 "name": "ta1",
                 "label": "多行文本",
                 "outerWidth": 12,
@@ -126,26 +107,25 @@ $$.each(pageTitles, function(idx){
                 "placeholder": "请输入文本",
                 "description": "请输入英文、数字、下划线"
             }],
-            "rules": {
-                "s1": {
-                    "required": true
-                },
-                "t1": {
-                    "required": true,
-                    "maxlength": 10,
-                    "minlength": 5
-                },
-                "s2": {
-                    "required": true,
-                    "maxlength": 2
-                },
-                "ta1": {
-                    "required": true,
-                    "maxlength": 200,
-                    "minlength": 0
-                }
+        "rules": {
+            "s1": {
+                "required": true
             },
-            "events": [
+            "t1": {
+                "required": true,
+                "maxlength": 10,
+                "minlength": 5
+            },
+            "s2": {
+                "required": true,
+                "maxlength": 2
+            },
+            "ta1": {
+                "required": true,
+                "maxlength": 200,
+                "minlength": 0
+            }},
+        "events": [
             {
                 "eventType": "valueChangeDisable",
                 "trigger": "s1",
@@ -155,15 +135,142 @@ $$.each(pageTitles, function(idx){
                     "3": "ta1"
                 }
             }],
-            "values": {
-                "s1": "1",
-                "t1": "测试文本",
-                "s2": ["2", "3"],
-                "ta1": "多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行"
+        "values": {
+            "s1": "1",
+            "t1": "测试文本",
+            "s2": ["2", "3"],
+            "ta1": "多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行多行"},
+        "isSteam": false,
+        "isRead": false},
+    'student-2':{
+        "items": [
+            {
+                "name": "s1",
+                "label": "下拉单选",
+                "outerWidth": 12,
+                "labelWidth": 3,
+                "contentWidth": 9,
+                "widthInSteam": "auto",
+                "type": "select",
+                "placeholder": "请点击选择",
+                "description": "请点击选择",
+                "dataUrl": "",
+                "options": [{
+                    "label": "选项1",
+                    "value": 1
+                }, {
+                    "label": "选项2",
+                    "value": 2
+                }, {
+                    "label": "选项3",
+                    "value": 3
+                }]
+            }, {
+                "name": "t1",
+                "label": "文本框",
+                "outerWidth": 12,
+                "labelWidth": 3,
+                "contentWidth": 9,
+                "widthInSteam": "auto",
+                "type": "text",
+                "placeholder": "请输入文本",
+                "description": "请输入英文、数字、下划线"
+            }, {
+                "name": "s2",
+                "label": "下拉多选",
+                "outerWidth": 12,
+                "labelWidth": 3,
+                "contentWidth": 9,
+                "widthInSteam": "auto",
+                "type": "multiselect",
+                "placeholder": "请点击选择",
+                "description": "请点击选择",
+                "dataUrl": "",
+                "options": [{
+                    "label": "选项1",
+                    "value": 1
+                },
+                {
+                    "label": "选项2",
+                    "value": 2
+                },
+                {
+                    "label": "选项3",
+                    "value": 3
+                }]
+            }, {
+                "name": "ta1",
+                "label": "多行文本",
+                "outerWidth": 12,
+                "labelWidth": 3,
+                "contentWidth": 9,
+                "widthInSteam": "auto",
+                "type": "textarea",
+                "rows": 3,
+                "resize": "none",
+                "placeholder": "请输入文本",
+                "description": "请输入英文、数字、下划线"
+            }],
+        "rules": {
+            "s1": {
+                "required": true
             },
-            "isSteam": false,
-            "isRead": false
-        });
+            "t1": {
+                "required": true,
+                "maxlength": 10,
+                "minlength": 5
+            },
+            "s2": {
+                "required": true,
+                "maxlength": 2
+            },
+            "ta1": {
+                "required": true,
+                "maxlength": 200,
+                "minlength": 0
+            }},
+        "events": [
+            {
+                "eventType": "valueChangeDisable",
+                "trigger": "s1",
+                "valueResps": {
+                    "1": "t1",
+                    "2": "s2",
+                    "3": "ta1"
+                }
+            }],
+        "values": {
+            "s1": "asdfasdfas",
+            "t1": "ccccccc",
+            "s2": ["1", "3"],
+            "ta1": ""},
+        "isSteam": false,
+        "isRead": false},
+    };
+
+// 当前页面的页码
+var nowPageIndex = 0;
+
+// 初始化页面和导航
+$$.each(pageTitles, function(idx){
+    var pt = pageTitles[idx];
+
+    // 导航菜单初始化
+    $$('.navList').append('<p><a class="close-panel" href="#' + pt.url + '">' + pt.label + '</a></p>');
+
+    // page初始化
+    var $page = $$(pageTemplate.format({formId: pt.url})).attr('data-page', pt.url)
+            .attr('page-index', idx).addClass('cached');
+
+    // TODO: 初始化页面详细过程
+    if (idx != 0) {
+        $$('.pages').append($page);
+
+        // 渲染form主体
+        $page.find('form').renderForm(jsonConfs[pt.url]);
+
+        // 加form的标题
+        $page.find('form').prepend('<div class="content-block-title">' + pt.label + '</div>');
     }
 
     // 翻页后事件回调绑定
