325972aa017f56c8a938d2de1afd190913ae4f94
yolo <kuroikenshi@sina.com>
2017-09-14 18:11:20 +0800

    Up f7-fb, audio add event, undone

--- ws/form_configurator/assets/fw7/js/my-app_4+1.js
+++ ws/form_configurator/assets/fw7/js/my-app_4+1.js
@@ -25,6 +25,7 @@ var mainView = myApp.addView('.view-main', {
 	animatePages: false
 });
 
+
 // 给安卓机械后退按键使用的方法
 function androidBackBtn() {
 	mainView.history.pop()
@@ -36,6 +37,70 @@ function androidBackBtn() {
 		window.location = '/exit-web-view';
 	}
 }
+
+
+// 自制播放器-播放/暂停事件绑定
+$('.audio-player i.ppBtn').on('click', function(e) {
+	var $audio = $(e.target).closest('.audio-player').find('audio');
+	var $this = $(e.target);
+	if ($this.html() == 'play_round') {
+		$audio[0].play();
+		$this.html('pause_round');
+	} else {
+		$audio[0].pause();
+		$this.html('play_round');
+	}
+});
+// 自制播放器-播放进度条-开始拖拽
+$('.audio-player .bar-control').on('touchstart', function(e) {
+	var $processBar = $(e.target).closest('.progress-bar');
+	var $playedPart = $processBar.find('.played-part');
+	var $audio = $(e.target).closest('.audio-player').find('audio');
+	var leftX = $processBar.offset().left + $processBar.parent().offset().left + 16;
+	var barWidth = $processBar.width();
+	var per = 0;
+	// 去掉更新进度条的事件绑定
+	$audio.off('timeupdate');
+	$(document).on('touchmove', function(e) {
+		per = (((e.touches[0].clientX - leftX) / barWidth) * 100).toFixed(2);
+		if (per < 0) {
+			per = 0;
+		} else if (per > 100) {
+			per = 100;
+		}
+		console.log(per + '%');
+		$playedPart.css('width', per + '%');
+	});
+});
+// 自制播放器-播放进度条-释放
+$('.audio-player .bar-control').on('touchend', function(e) {
+	$(document).off('touchmove');
+	console.log('touch-end');
+	var $processBar = $(e.target).closest('.progress-bar');
+	var $playedPart = $processBar.find('.played-part');
+	var endPer = ($playedPart.width() / $processBar.width() * 100).toFixed(2);
+	console.log('end at', endPer + '%');
+	// TODO: 跳转到指定位置播放
+	// TODO: 绑定更新事件
+});
+// 自制播放器-更新进度条和剩余时间
+$('.audio-player audio').on('timeupdate', function(e) {
+	var $this = $(e.target);
+	var scales = $this[0].currentTime / $this[0].duration;
+	var leftSeconds = parseInt($this[0].duration - $this[0].currentTime);
+	var persentNum = (scales * 100).toFixed(2);
+	var $bar = $this.parent().find('.played-part');
+	var $time = $this.parent().find('.time');
+	
+	$bar.css('width',  persentNum + '%');
+	$time.html(formatTime(leftSeconds));
+});
+// 秒数转“分:秒”
+function formatTime(second) {
+    return [/*parseInt(second / 60 / 60), */parseInt(second / 60 % 60), parseInt(second % 60)].join(":")
+        .replace(/\b(\d)\b/g, "0$1");
+}
+
 
 // 扩展array类型原生方法，添加obj如果是array，就让其元素合并，否则直接加入
 Array.prototype.add = function(obj) {
