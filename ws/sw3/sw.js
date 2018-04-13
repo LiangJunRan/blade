4c85de4b84aae5cead6966f34064a07a4f71b5ba
yolo <kuroikenshi@sina.com>
2018-04-13 09:09:47 +0800

    Add sw3, new try, not done yet

--- ws/sw3/sw.js
+++ ws/sw3/sw.js
@@ -2,29 +2,57 @@
 var CACHE_NAME = 'sw3';
 // 缓存文件列表
 var urlsToCache = [
-  "/controlled/"
+  "/controlled"
 ];
 var host = 'http://localhost:9494';
 var entryUrlList = [
   "/entry.html"
 ];
-var versionUrl = '/controlled/version.json';
+
+// 循环定时任务
+var loopTask = undefined;
+
+
+Date.prototype.Format = function (fmt) { // author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, // 月份
+        "d+": this.getDate(), // 日
+        "h+": this.getHours(), // 小时
+        "m+": this.getMinutes(), // 分
+        "s+": this.getSeconds(), // 秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+        "S": this.getMilliseconds() // 毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            return fmt;
+}
+function gdt() {
+  return (new Date()).Format("[dd HH:mm:ss]"); 
+}
 
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('打开缓存');
+        console.log('SW_INSTALL...');
         return cache.addAll(urlsToCache);
+      })
+      .then(function() {
+        console.log('SW_INSTALL_R', gdt());
+        return Promise.resolve();
       })
   );
 });
 
 
-var checkVersionDetail = data => {
-
-}
+// 监听激活
+this.addEventListener("activate", function(event) {
+  console.log("SW_ACTIVATE", gdt());
+});
 
 
 self.addEventListener('fetch', function(evt) {
@@ -35,7 +63,7 @@ self.addEventListener('fetch', function(evt) {
   if (entryUrlList.indexOf(path) !== -1) {
     console.log('请求入口页，检查version');
 
-    // evt.waitUntil(
+    evt.waitUntil(
       /* 
         先取到json，然后利用#A.return进入#C
         顺序是:
@@ -52,56 +80,38 @@ self.addEventListener('fetch', function(evt) {
           -> ...
         ...
       */
-      fetch(versionUrl, {cache: 'no-cache'})
-        .then(r => {                            // #A
-          // 检查ETag
-          console.log('headers:', r.headers);
-          let etag = r.headers.get('ETag');
-          console.log('etag:', etag);
-          return (new Promise((resolve, reject) => {
-            r.json()
-              .then(dt => {                     // #B
-                resolve([dt, etag]);
-              })
-          }));
-        })
-        .then((data) => {                       // #C
-          console.log('data:', data);
-          console.log(' |-- jsonData:', data[0]);
-          console.log(' |-- etag:', data[1]);
-
-          // 将数据发送给client
-          return self.clients.matchAll().then(function(clients) {
-            clients.forEach(function(client) {
-              var message = {
-                type: 'versionCheck',
-                url: path,
-                data: {
-                  content: data[0],
-                  etag: data[1]
-                }
-              };
-
-              client.postMessage(JSON.stringify(message));
-            })// END of forEach
-          });// END of return
-        });
-        // END of fetch
-    // );// END of evt.waitUntil
+      
+    );// END of evt.waitUntil
   }
 
-  // event.respondWith(
-  //   caches.match(event.request)
-  //     .then(function(response) {
-  //       // Cache hit - return response
-  //       if (response) {
-  //         console.log('sw>> [CACHE-HIT] 找到缓存，返回响应', event.request.url);
-  //         return response;
-  //       }
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          console.log('sw>> [CACHE-HIT] 找到缓存，返回响应', evt.request.url);
+          return response;
+        }
 
-  //       console.log('sw>> [FATCH] 未找到缓存，从服务器取得', event.request.url);
-  //       return fetch(event.request);
-  //     }
-  //   )
-  // );
+        console.log('sw>> [FATCH] 未找到缓存，从服务器取得', evt.request.url);
+        return fetch(evt.request);
+      }
+    )
+  );
 });
+
+self.addEventListener('message', function(evt) {
+  console.log('SW_RECV-MSG>>>', evt.data);
+  var msg = JSON.parse(evt.data);
+
+  switch (msg.type) {
+    case 'stopSend_versionCheck':
+      clearInterval(loopTask);
+      console.log('msg -> SUCCESS !!!!!');
+      break;
+    default:
+      console.log('[WARN] msg.type: "' + msg.type + '", nothing to match');
+      break;
+  }
+});
+
