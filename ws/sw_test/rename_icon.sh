7ea41d03ab1013cc47e403f4e000d0851f3dabb0
yolo <kuroikenshi@sina.com>
2018-06-14 18:02:24 +0800

    Up sw_test, test update sw not effect

--- ws/sw_test/rename_icon.sh
+++ ws/sw_test/rename_icon.sh
@@ -1,3 +1,7 @@
-mv icon1.png icon2.png
-mv iconT.png icon1.png
-mv icon2.png iconT.png
+mv controlled/icon1.png controlled/icon2.png
+mv controlled/iconT.png controlled/icon1.png
+mv controlled/icon2.png controlled/iconT.png
+
+mv uncontrolled/icona1.png uncontrolled/icona2.png
+mv uncontrolled/iconaT.png uncontrolled/icona1.png
+mv uncontrolled/icona2.png uncontrolled/iconaT.png
--- ws/sw_test/sw.js
+++ ws/sw_test/sw.js
@@ -1,8 +1,8 @@
 var CACHE_NAME = 'test';
 
 var urlsToCache = [
-	"/icon1.png",
-	"/iconT.png"
+	"/controlled/sub-2.html",
+	"/controlled/icon1.png"
 ];
 
 
@@ -13,12 +13,15 @@ var urlsToCache = [
 self.addEventListener('install', function(event) {
 	console.log('The service worker is being installed.');
 
-	event.waitUntil(preCache());
+	event.waitUntil(preCache().then(function() {
+		console.log('[ServiceWorker] Skip waiting on install');
+    	return self.skipWaiting();
+	}));
 });
 
 function preCache() {
 	return caches.open(CACHE_NAME).then(function (cache) {
-		console.log('SW_INSTALL...');
+		console.log('SW_INSTALL...preCached');
 		return cache.addAll(urlsToCache);
 	});
 }
@@ -30,8 +33,6 @@ function preCache() {
 // FETCH
 self.addEventListener('fetch', function(evt) {
 	console.log('The service worker is serving the asset.');
-
-	console.log(evt);
  
 	evt.respondWith(fromCacheFirstOrFetch(evt.request));
 });
@@ -50,8 +51,12 @@ function fromCacheFirstOrFetch(request) {
 	return caches.open(CACHE_NAME).then(function (cache) {
 		return cache.match(request).then(function (matchedResponse) {
 			if (matchedResponse) {
+				console.log('  -> from cache', request.url);
 				return matchedResponse;		// 从cache中取出
 			} else {
+				console.log('  -> from fetch', request.url);
+				// 不加no-cache会load from memory cache
+				// 加了no-cache会报错：Uncaught (in promise) TypeError: Failed to execute 'fetch' on 'ServiceWorkerGlobalScope': Cannot construct a Request with a Request whose mode is 'navigate' and a non-empty RequestInit.
 				return fetch(request);		// 从fetch中取出（TODO: 新资源？是否会被浏览器缓存？NO-CACHE？）
 			}
 			
